029ea4b6a70baec0e8dd35988e795792c64ecbc7
Suhaib Al-Tamimi <suhaib.altamimi3@gmail.com>
2017-09-17 20:30:37 -0700

    updated the code to use a database, corrected functions
    I have updated the code to use sqlite3 as a database to insert timestamps and pull them to display in a label for users. 
    I have updated the code to side step future errors the user might encounter if they cancel the filedialog.askdirectory(). 
    I have created a function that will pull the timestamp only if this program has ran previously.

--- Daily_file_transfer/daily_file_transfer_func.py
+++ Daily_file_transfer/daily_file_transfer_func.py
@@ -10,17 +10,15 @@
 
 import os
 import shutil
-import time
+import time, datetime
 from tkinter import *
 from tkinter import messagebox, filedialog
+import sqlite3
 import daily_file_transfer_main
 import daily_file_transfer_gui
 
 # Below is the function that transfers created and modified files from folder A
 # to folder B 
-source = '/Users/suhaibabdul-sahib/desktop/folderA/'
-destination = '/Users/suhaibabdul-sahib/desktop/folderB/'
-
 
 def copy_files(self):
     if (len(self.entry_source.get()) == 0):
@@ -33,9 +31,12 @@ def copy_files(self):
         src = self.entry_source.get()+'/'
         dest = self.entry_destination.get()+'/'
         for files in os.listdir(src):
-           if time.time() - os.path.getmtime(source + files) <= 86400:
-               if files.endswith(".txt"):
+           if time.time() - os.path.getmtime(src+files) <= 86400:
+               if files.endswith(".rtf"):
                    shutil.copy(src +files, dest)
+        create_db(self)
+        record_timestamp(self)
+        update_entry(self)
         update_list2(self)
  
                 
@@ -43,26 +44,32 @@ def copy_files(self):
 # This is the function section to open the transfer from folder where a folder is selected
 # then the path is printed in the source entry section in the GUI 
 def open_src(self):
-    self.entry_source.delete(0,'end') # incase there was a previouse folder path in the entry box this command will delete it.
-    self.list1.delete(0,'end') # this code is to delete the list box if the user selects a fold and then goes back and selects another folder.
-    scr = filedialog.askdirectory()
-    self.entry_source.insert(0,scr)
-    for files in os.listdir(scr):
-        if time.time() - os.path.getmtime(source + files) <= 86400:
-            if files.endswith(".txt"):
-                self.list1.insert('end', files +'\n')
+    try:
+        self.entry_source.delete(0,'end') # incase there was a previouse folder path in the entry box this command will delete it.
+        self.list1.delete(0,'end') # this code is to delete the list box if the user selects a fold and then goes back and selects another folder.
+        scr = filedialog.askdirectory()
+        self.entry_source.insert(0,scr)
+        for files in os.listdir(scr):
+            sourceFile = os.path.join(scr,files)
+            if time.time() - os.path.getmtime(sourceFile) <= 86400:
+                if files.endswith(".rtf"):
+                    self.list1.insert('end', files +'\n')
+    except:
+        pass  # This try and except is so that when the users cancels the program will not through an error.
 
 
 # This is the function section to open the transfer from folder where a folder is selected
 # then the path is printed in the source entry section in the GUI 
 def open_dest(self):
-    self.entry_destination.delete(0,'end') # incase there was a previouse folder path in the entry box this command will delete it.
-    self.list2.delete(0,'end') # this code is to delete the list box if the user selects a fold and then goes back and selects another folder.
-    dest = filedialog.askdirectory()
-    self.entry_destination.insert(0,dest)
-    for files in os.listdir(dest):
-        self.list2.insert('end', files +'\n') # will display each file on a new lin in the list
-
+    try:
+        self.entry_destination.delete(0,'end') # incase there was a previouse folder path in the entry box this command will delete it.
+        self.list2.delete(0,'end') # this code is to delete the list box if the user selects a fold and then goes back and selects another folder.
+        dest = filedialog.askdirectory()
+        self.entry_destination.insert(0,dest)
+        for files in os.listdir(dest):
+            self.list2.insert('end', files +'\n') # will display each file on a new lin in the list
+    except:
+        pass  
 
 
 def update_list2(self):
@@ -80,8 +87,40 @@ def close_prog(self):
                         # to prevent a memory leak.
 
 
+
+# This section is the function that creates a database and a table if it doesn't already exsist
+def create_db(self):
+    conn = sqlite3.connect('transfer_record.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS tbl_file_transfer(ID INTEGER PRIMARY KEY AUTOINCREMENT, Timestamp TEXT);")
+    conn.commit()
+    conn.close()
+
+# This function is to insert the timestamp of the last time the files where copied
+def record_timestamp(self):
+    last_update = datetime.datetime.now().strftime('%m-%d-%Y, %H:%M:%S') 
+    conn = sqlite3.connect('transfer_record.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute('''INSERT INTO tbl_file_transfer (Timestamp) VALUES(?)''', (last_update,))
+    conn.commit()
+    conn.close()
+
+
+def update_entry(self):
+    try:
+        conn = sqlite3.connect('transfer_record.db')
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT MAX(timestamp) FROM tbl_file_transfer;")
+            varTimestamp = cur.fetchone()[0]
+            self.lbl_ltransfer.config(text = "Last update was: {}".format(varTimestamp))
+        conn.close()
+    except:
+        pass
+
 if __name__ == "__main__":
     root = tk.Tk() 
     App = ParentWindow(root) 
     root.mainloop()
-
